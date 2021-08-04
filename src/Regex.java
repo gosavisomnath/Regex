0ed1ce947308f71b0848ad4c3d4bf3b265b0b31e
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-04 10:14:11 +0530

    First Name is Valid or not

--- src/Regex.java
+++ src/Regex.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
+
+    // For First Name
+    public static void AddFirstName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a First Name:");
+        String name = sc.nextLine();
+        System.out.println("First Name is : " + name);
+        Pattern pattern = Pattern.compile("^([A-Z]{1})([a-z]{2,})$");
+        Matcher matcher = pattern.matcher(name);
+        boolean MatchFound = matcher.matches();
+
+        if (MatchFound) {
+            System.out.println("Name is Valid");
+        } else {
+            System.out.println("Name is invalid");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To The User Registration Program");
+        AddFirstName();
     }
 }
